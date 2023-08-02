8f62b5f6168744c9759bdfca278ddf01807c5233
Ulysse Charraud <ulysse@ik.me>
2023-08-02 10:17:44 +0200

    modif images

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,5 @@
 // Entry point for the build script in your package.json
 //= require jquery
 
-import "@hotwired/turbo-rails"
-import "./controllers"
+import "@hotwired/turbo-rails";
+import "./controllers";
